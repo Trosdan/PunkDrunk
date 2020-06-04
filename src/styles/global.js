cbe2644738dd1d8dbfeaacf428c66f51050779d8
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-06-04 19:25:42 -0300

    add github pages

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 
   * {
     margin: 0;
