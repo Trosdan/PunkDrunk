bca34554cda1cfcc3f47d9c82f88d503b0323388
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-06-04 19:51:05 -0300

    Updates

---
+++ precache-manifest.eba3a5c9f73c1f46beef4356a7186291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfc6dfb97d1685fcdc5bbf756ebec04",
+    "url": "/PunkDrunk/index.html"
+  },
+  {
+    "revision": "f0ffcc685a1cfed13f24",
+    "url": "/PunkDrunk/static/js/2.e50d186c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PunkDrunk/static/js/2.e50d186c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319467f893bcceda61ed",
+    "url": "/PunkDrunk/static/js/main.dc39ac72.chunk.js"
+  },
+  {
+    "revision": "9a6da00163737659cc85",
+    "url": "/PunkDrunk/static/js/runtime-main.4a5f7a9f.js"
+  },
+  {
+    "revision": "b373ac55f518aec72080e4943d34845c",
+    "url": "/PunkDrunk/static/media/logo.b373ac55.png"
+  }
+]);
